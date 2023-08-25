c147b3463c1384725ba07f6edca5f5f656667177
Gabriel Alencar de Araujo <dev.alencar.gabriel@gmail.com>
2023-08-25 12:46:33 -0300

    .

--- src/util/InterfaceUsuario.java
+++ src/util/InterfaceUsuario.java
@@ -6,52 +6,49 @@ public class InterfaceUsuario implements InterUsuario {
     private Scanner inputUser = new Scanner(System.in);
 
     public double inputValorImovel(){
-        double valorImovel;
+        double valorImovel = 0;
 
-        while(true){
+        while (valorImovel <= 0) {
             System.out.print("Digite o valor do imovel: ");
             valorImovel = inputUser.nextDouble();
             
-            if(valorImovel <= 0) {
-                System.out.println("Valor do imovel nao pode ser menor ou igual a zero.\n");
-                continue;
-            } 
+            if (valorImovel > 0) break;
 
-            return valorImovel;
-        }
+            System.out.println("O valor deve ser maior que zero.\n");
+        } ;
+        
+        return valorImovel;
     }
 
     public int inputPrazoFinanciamento(){
         int prazoFinanciamento;
-
-        while(true) {
+        
+        do {
             System.out.print("Digite o prazo de Financiamento em anos: ");
             prazoFinanciamento =  inputUser.nextInt();
+    
+            if(prazoFinanciamento <= 0) System.out.println("O valor deve ser maior que zero.\n");
+               
+        } while(prazoFinanciamento <= 0);
 
-            if(prazoFinanciamento < 0) {
-                System.out.println("O prazo nao deve ser menor ou igual a zero.\n");
-                continue;
-            } 
-
-            return prazoFinanciamento;
-        } 
+        return prazoFinanciamento;
     }
 
     public double inputTaxaJuros(){
         double taxaJuros;
-
-        System.out.print("Digite a taxa de juros anual: ");
-        taxaJuros = inputUser.nextDouble();
-        
-        if(taxaJuros < 0) {
-            System.out.println("A taxa de juros nao pode ser menor que zero.\n");
-            this.inputTaxaJuros();
-        } 
+        do {
+            System.out.print("Digite a taxa de juros anual: ");
+            taxaJuros = inputUser.nextDouble();
+            
+            if(taxaJuros <= 0) System.out.println("A taxa de juros nao pode ser menor que zero.\n");
+            
+        } while (taxaJuros <= 0);
         return taxaJuros;
     }
 
     public static void resultFinanciamento(double pagamentoMensal, double pagamentoTotal){
         System.out.println("\n O pagamento mensal do imovel: " + pagamentoMensal);
         System.out.println("O pagamento total do imovel: " + pagamentoTotal);
+        System.out.println("");
     }
 }
