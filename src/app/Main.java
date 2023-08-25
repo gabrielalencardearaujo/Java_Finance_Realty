839be658e72e9f45c7b912b321c132449de4fd20
Gabriel Alencar de Araujo <dev.alencar.gabriel@gmail.com>
2023-08-25 17:15:53 -0300

    Conclusao Somativa - Semana 4

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,35 +1,51 @@
 package app;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 import modelo.*;
 import util.*;
 
 public class Main {
     public static Scanner input = new Scanner(System.in);
-
+    public static ArrayList<Financiamento> financiamentos = new ArrayList<Financiamento>();
+    public static double totalFinanciamentos, totalImoveis;
     public static void main(String[] args) {
-        System.out.println("Bem vindo ao Sistema de financiamento de Imoveis: ");
+        System.out.println("Bem vindo ao Sistema de financiamento de Imoveis: \n");
+        String controlador;
+        
+        do {
+            System.out.println("Insira os dados abaixo \n");
+            Financiamento fin = startApp();
+            financiamentos.add(fin);
+            
+            System.out.print("\nGostaria de simular um financiamento? Y/N  ");
+            controlador = input.nextLine().toUpperCase();
+        } while (controlador.equals("Y"));
 
-        System.out.print("Gostaria de simular um financiamento? Y/N  ");
-        String inputChoiceUser = input.nextLine().toUpperCase();
-        System.out.println(inputChoiceUser);
+        int cont = 1;
+        for(Financiamento fin: financiamentos) {
+            System.out.println("\nFinanciamento " + cont + " - valor do Imovel: R$" + fin.getValorImovel() + ", valor total do financiamento: R$" + fin.totalPagamento());
 
-        if(inputChoiceUser.equals("Y")) {
-            startApp();
-        } else {
-            System.out.println("Obrigado por nos escolher! Tenha um bom dia!");
+            totalImoveis += fin.getValorImovel();
+            totalFinanciamentos += fin.totalPagamento();
+
+            cont++;
         }
+
+        System.out.println("\nValor total dos imoveis: R$" + totalImoveis);
+        System.out.println("Valor total dos financiamentos: R$" + totalFinanciamentos);
     }
 
-    public static void startApp(){
+    public static Financiamento startApp(){
         InterfaceUsuario user = new util.InterfaceUsuario();
 
         double valorImovel = user.inputValorImovel();
         int prazoFinanciamento = user.inputPrazoFinanciamento();
         double taxaJuros = user.inputTaxaJuros();
-
+        
         Financiamento financiamento = new modelo.Financiamento(valorImovel, prazoFinanciamento, taxaJuros);
 
-        InterfaceUsuario.resultFinanciamento(financiamento.calcPagamentoMensal(), financiamento.totalPagamento());
+        return financiamento;
+        // InterfaceUsuario.resultFinanciamento(financiamento.calcPagamentoMensal(), financiamento.totalPagamento());
     }
 }
